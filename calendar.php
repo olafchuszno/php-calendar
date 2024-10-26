dcb694759ff1586ea6f57c9cec8b7782ee744ec6
Olaf <olaf.chuszno@gmail.com>
2024-10-26 14:16:27 +0200

    Implement basic calendar

--- calendar.php
+++ calendar.php
@@ -6,27 +6,40 @@ $calendar = [[]];
 
 $days_in_month = 30;
 
-$start_day_index = 0;
+$start_day_index = 1;
 
 $current_day_of_month = 1;
-$current_day_of_week = 0;
+$current_day_of_week = 1;
 $current_week_of_month = 0;
 
-for ($i = 0; $i < $days_in_month; $i++) {
+for ($i = 0; $i < $days_in_month + $start_day_index; $i++) {
   if ($i < $start_day_index) {
     $calendar[$current_week_of_month][] = null;
+
+    // Dodaliśmy dzień, więc uzupełniamy counter
+    $current_day_of_week++;
+
     continue;
   }
 
   // Jeśli jest miejsce w tym tygodniu
-  if (true) {
+  if ($current_day_of_week <= 7) {
     $calendar[$current_week_of_month][] = $current_day_of_month;
   } else {
-    // Przejdź do kolejnego tygodnia i dodaj dzień
+    // Przejdź do kolejnego tygodnia
     $current_week_of_month++;
+
+    // Dodaj tablicę nowego tygodnia
+    $calendar[] = [];
+
+    // Wyzeruj dzień tygodnia na 1 (poniedziałek)
+    $current_day_of_week = 1;
+
+    // Wypełnij dzień w kalendarzu
     $calendar[$current_week_of_month][] = $current_day_of_month;
   }
 
+  $current_day_of_week++;
   $current_day_of_month++;
 }
 
@@ -42,13 +55,17 @@ for ($i = 0; $i < $days_in_month; $i++) {
 <body>
   <table>
     <thead>
-      <tr>
         <?php 
-          foreach ($days_of_week as $day) {
-            echo "<th>$day</th>";
+          foreach ($calendar as $week) {
+            echo "<tr>";
+            
+            foreach ($week as $day) {
+              echo isset($day) ? "<td>$day</td>" : '<td></td>';
+            }
+
+            echo "</tr>";
           }
-        ?>
-      </tr>
+        ?> 
     </thead>
     <tbody>
       <tr></tr>
