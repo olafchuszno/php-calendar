4f2fde68a49538cdafbb6601f6fed4f9457e088e
Olaf <olaf.chuszno@gmail.com>
2024-10-26 14:15:57 +0200

    Write task structure

--- calendar.php
+++ calendar.php
@@ -2,6 +2,34 @@
 
 $days_of_week = ['Pn', 'Wt', 'Śr', 'Cz', 'Pt', 'SO', 'N'];
 
+$calendar = [[]];
+
+$days_in_month = 30;
+
+$start_day_index = 0;
+
+$current_day_of_month = 1;
+$current_day_of_week = 0;
+$current_week_of_month = 0;
+
+for ($i = 0; $i < $days_in_month; $i++) {
+  if ($i < $start_day_index) {
+    $calendar[$current_week_of_month][] = null;
+    continue;
+  }
+
+  // Jeśli jest miejsce w tym tygodniu
+  if (true) {
+    $calendar[$current_week_of_month][] = $current_day_of_month;
+  } else {
+    // Przejdź do kolejnego tygodnia i dodaj dzień
+    $current_week_of_month++;
+    $calendar[$current_week_of_month][] = $current_day_of_month;
+  }
+
+  $current_day_of_month++;
+}
+
 ?>
 
 <!DOCTYPE html>
