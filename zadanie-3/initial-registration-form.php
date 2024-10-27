b5144536daea6f20c80d5b25c3266dba194aeac3
Olaf <olaf.chuszno@gmail.com>
2024-10-27 11:33:59 +0100

    Remove redundant script from initial registration form

--- zadanie-3/initial-registration-form.php
+++ zadanie-3/initial-registration-form.php
@@ -4,47 +4,8 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
+  <title>Wybór rodzaju konta</title>
   <script src="https://cdn.tailwindcss.com"></script>
-  <script>
-    // Walidacja danych
-    document.getElementById('registration-form').addEventListener('submit', (event) => {
-      const userType = document.getElementById('user-type').value;
-
-      const userTypes = ['private-person', 'company'];
-
-      if (userType === 'none') {
-        alert('Wybierz rodzaj konta.');
-
-        event.preventDefault();
-        return;
-      }
-
-      const birthDateInput = document.querySelector('input[name="date_of_birth"]');
-
-      if (!birthDateInput) {
-        alert('Data urodzenia musi zostać podana.')
-        event.preventDefault();
-        return;
-      }
-
-      const usersBirthDate = birthDateInput.value;
-
-      const now = new Date();
-
-      const currentDate = now.getDate();
-      const currentMonth = now.getMonth() + 1;
-      const currentYear = now.getFullYear();
-
-      const dateOfLegalAge = `${currentYear - 18}-${currentMonth}-${currentDate}`;
-
-      if (dateOfLegalAge < usersBirthDate) {
-        alert('Tylko osoby pełnoletnie mogą utworzyć konto.');
-        event.preventDefault();
-      }
-
-    })
-  </script>
 </head>
 
 <body class="bg-gray-100 flex items-center justify-center min-h-screen">
